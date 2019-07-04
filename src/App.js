a1c866081f35f829a47126f7855bc1bd2911416f
Shilpi <shilpi@Mohds-MacBook-Pro.local>
2019-07-04 15:46:40 +0530

    25 The React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
+import Navbar from "./component/navbar";
+import Home from "./component/home";
+import About from "./component/about";
+import Contact from "./component/contact";
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (     
+      <BrowserRouter>      
+        <div className="container">
+          <header className="App-header">
+            <Navbar/>
+            <Route exact path='/' component={Home}/>
+            <Route path='/about' component={About}/>
+            <Route path='/contact' component={Contact}/>
+          </header>
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
-
 export default App;
---
+++ src/component/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+           <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+           </ul>
+        </div>
+    )
+}
+export default Navbar;
